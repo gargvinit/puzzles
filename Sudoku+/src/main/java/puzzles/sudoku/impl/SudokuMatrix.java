0db547076ff2bb8380283719aca5796ba215eeb9
Vinit <vgarg@tagged.com>
2014-10-12 19:51:57 -0700

    Added test cases to test for existing valid and invalid use cases. It would be nice to be able to go through the directory but without spring dependency, it could be tricky. Hardcoding the files for now

--- Sudoku+/src/main/java/puzzles/sudoku/impl/SudokuMatrix.java
+++ Sudoku+/src/main/java/puzzles/sudoku/impl/SudokuMatrix.java
@@ -6,7 +6,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import javax.swing.text.html.HTMLDocument.HTMLReader.SpecialAction;
 
 public class SudokuMatrix {
 
---
+++ Sudoku+/src/test/java/puzzles/sudoku/SudokuPlusFilesTest.java
@@ -0,0 +1,51 @@
+package puzzles.sudoku;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import puzzles.sudoku.impl.SudokuMatrix;
+import junit.framework.TestCase;
+
+public class SudokuPlusFilesTest extends TestCase {
+	public void testValidInputs() throws IOException {
+		String[] validFiles = { "1x1", "4x4", "9x9" };
+		for (String validFile : validFiles) {
+			SudokuMatrix matrix = getMatrix("validInput " + validFile);
+			assertTrue(validFile + " must be valid", matrix.isValid());
+			assertTrue(
+					validFile + "must not have any issues " + matrix.issues(),
+					matrix.issues().isEmpty());
+		}
+	}
+
+	public void testInvalidInputs() throws IOException {
+		String[] validFiles = { "TextualEnglish", "SizeMismatch",
+				"NumbersNotMatching", "NumbersNotInRange",
+				"NonNumericCharacter", "Empty", "ElementsAre0",
+				"DuplicateEntries", "2x2" };
+		for (String invalidFile : validFiles) {
+			SudokuMatrix matrix = getMatrix("invalidInput" + invalidFile);
+			assertFalse(invalidFile + "must not be valid", matrix.isValid());
+			assertFalse(invalidFile + " must have issues", matrix.issues()
+					.isEmpty());
+		}
+	}
+
+	private SudokuMatrix getMatrix(String fn) throws IOException {
+		return new SudokuMatrix(readFile(fn), ",");
+	}
+
+	private List<String> readFile(String fn) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(this
+				.getClass().getResourceAsStream("/" + fn + ".txt")));
+		List<String> contents = new ArrayList<String>();
+		String content;
+		while ((content = br.readLine()) != null) {
+			contents.add(content);
+		}
+		return contents;
+	}
+}
